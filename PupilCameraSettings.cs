c5e0205b9e141c8aceb7da8f18848925b6fb53de
Tristan Damron <TristanDamron31@gmail.com>
2018-02-05 13:14:07 -0800

    Changed StereoScaling.prefab to PupilCameraSettings.prefab and made PupilCamera adjustment settings variable.

---
+++ PupilCameraSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Pupil;
+
+public class PupilCameraSettings : MonoBehaviour {
+	private PupilCamera _camera;
+	[SerializeField]
+	private float _minDistanceIPD;
+	[SerializeField]
+	private float _minDistance;
+	[SerializeField]
+	private float _maxDistanceIPD;
+	[SerializeField]
+	private float _maxDistance;
+	[SerializeField]
+	private bool _debug;
+
+	void Start () {
+		_camera = new PupilCamera();		 	
+
+		//Set Pupil Settings
+		_camera.SetMinDistanceIPD(_minDistance, _minDistanceIPD);
+		_camera.SetMaxDistanceIPD(_maxDistance, _maxDistanceIPD);
+	}
+
+	void Update () {
+		_camera.AutoAdjustIPD();
+		if (_debug) 
+			_camera.DrawViewLines();
+	}
+}
